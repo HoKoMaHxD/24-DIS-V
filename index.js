b009a27f07780a39a6dce25c9df94cde304b5079
HoKoMaHxD <109537378+HoKoMaHxD@users.noreply.github.com>
2026-08-01 01:00:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const { Client } = require('discord.js-selfbot-v13');
-const VideoModule = require('@dank074/discord-video-stream');
+const { Streamer } = require('@dank074/discord-video-stream');
 
-// هذا السطر السحري سيقوم باستخراج أداة البث بغض النظر عن إصدار المكتبة
-const Streamer = VideoModule.Streamer || VideoModule.default || VideoModule;
-
-// إعداد صفحة الويب لمنع السكون
+// إعداد خادم الويب لمنع السكون
 const app = express();
-app.get('/', (req, res) => res.send('Video Stream is Live 24/7!'));
+app.get('/', (req, res) => res.send('Bot is Streaming 24/7!'));
 app.listen(process.env.PORT || 3000, () => console.log('Web server is ready!'));
 
-// إعداد حساب الديسكورد
+// إعداد الحساب
 const client = new Client({ checkUpdate: false });
+const streamer = new Streamer(client);
 
 const TOKEN = process.env.TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
@@ -21,9 +19,6 @@ const VIDEO_PATH = "./video.mp4";
 client.on('ready', async () => {
     console.log(`[+] Logged in as ${client.user.tag}`);
     try {
-        // نقلنا هذا السطر إلى هنا لضمان اكتمال تحميل الحساب قبل تشغيل الفيديو
-        const streamer = new Streamer(client);
-        
         await streamer.joinVoice(GUILD_ID, CHANNEL_ID);
         const stream = streamer.createStream();
         stream.playVideo(VIDEO_PATH, true); // true لتكرار الفيديو
